a56479e4094d4c977e0a0eaa0c9d8ce2c91ba843
Korta-n <lappokirill@gmail.com>
2021-11-02 21:34:46 +0300

    Removing unnecessary components

--- ui/components/registration/forms/paymentDetailsForm/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const DetailsContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: 77px;
-  grid-row-gap: 22px;
-`;
