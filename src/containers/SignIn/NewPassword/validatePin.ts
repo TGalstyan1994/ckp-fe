e2ec17ba8790b7947d38b75626b0be552ad8137c
Tigran <t.papikyan@rocketecsh.it>
2022-01-18 14:18:09 +0400

    Changed message "Field is required" to "Security PIN can't be less then 6 digits"

--- src/containers/SignIn/NewPassword/validatePin.ts
+++ src/containers/SignIn/NewPassword/validatePin.ts
@@ -12,8 +12,5 @@ export const validatePin = ({ securityCode }: FormState): ErrorObject => {
   if (!is.empty(securityCode) && securityCode.length < 6)
     errors.securityCode = "Security PIN can't be less then 6 digits"
 
-  if (is.empty(securityCode)) {
-    errors.securityCode = 'Field is required'
-  }
   return errors
 }
