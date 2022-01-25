8675d39a7d825c0b9b013d559b5919be48669369
Tigran <t.papikyan@rocketech.it>
2022-01-25 17:14:00 +0400

    side menu for admin

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -14,6 +14,8 @@ import ArrowOpenIcon from '../../assets/images/icons/arrow-open-icon'
 import LogoutIcon from '../../assets/images/icons/logout-icon'
 import { logOut } from '../../store/reducers/signin'
 import { LinkText } from '../LinkText'
+import { useSelectorTyped } from '../../utils/hooks'
+import { RootState } from '../../store'
 
 interface IMenuItem {
   svg: JSX.Element
@@ -26,7 +28,80 @@ interface IMenuItem {
   pathname: string
 }
 
-const menuItems: Array<IMenuItem> = [
+const adminMenuItems: Array<IMenuItem> = [
+  {
+    svg: <HomeIcon />,
+    name: 'Dashboard',
+    clickable: true,
+    url: '',
+    pathname: '/dashboard',
+  },
+  {
+    svg: <GlobeIcon />,
+    name: 'Universe',
+    clickable: false,
+    children: [
+      { field: 'Planet tree' },
+      { field: 'Sponsor tree' },
+      { field: 'Referral list' },
+    ],
+    pathname: '/universe',
+  },
+  {
+    svg: <MoneyBoxIcon />,
+    name: 'Donations',
+    clickable: false,
+    children: [
+      { field: 'Donations' },
+      { field: 'Cycle fee' },
+      { field: 'Relief fund' },
+      { field: 'Membership fee' },
+      { field: 'Waiting room' },
+    ],
+    pathname: '/donation',
+  },
+  {
+    svg: <MortarboardIcon />,
+    name: 'Academy',
+    clickable: true,
+    url: '',
+    pathname: '/academy',
+  },
+  {
+    svg: <EMailIcon />,
+    name: 'Communication',
+    clickable: true,
+    url: '',
+    pathname: '/communication',
+  },
+  {
+    svg: <SupportIcon />,
+    name: 'Support',
+    clickable: false,
+    children: [
+      { field: 'KYC' },
+      { field: 'Payment settings' },
+      { field: 'Feedback' },
+    ],
+    pathname: '/support',
+  },
+  {
+    svg: <ReportsIcon />,
+    name: 'Reports',
+    clickable: true,
+    url: '',
+    pathname: '/activities',
+  },
+  {
+    svg: <UserIcon />,
+    name: 'My Profile',
+    clickable: true,
+    url: '',
+    pathname: '/profile',
+  },
+]
+
+const userMenuItems: Array<IMenuItem> = [
   {
     svg: <HomeIcon />,
     name: 'Dashboard',
@@ -104,12 +179,43 @@ export const SideMenu: FC = () => {
     openSidebar: false,
   })
 
+  const { isSuperAdmin } = useSelectorTyped(
+    (state: RootState) => state.GlobalConfigDataStore
+  )
+
   const router = useRouter()
 
   const dispatch = useDispatch()
 
   const toggleSideBar = () => {
     setIsOpen({ openSidebar: !isOpen.openSidebar })
+  }
+
+  const handleItems = (items: Array<IMenuItem>) => {
+    return items.map((item: IMenuItem) => (
+      <LinkText href={item.pathname} key={item.pathname}>
+        <div
+          className={
+            router.pathname === item.pathname ? 'active-icons' : 'icon'
+          }
+        >
+          <span className="svgIcon">{item.svg}</span>
+          <span className="name">{item.name}</span>
+          <span className="arrow">{item.children && <ArrowOpenIcon />}</span>
+          {item.children && (
+            <>
+              <ul className={item.children?.length ? 'fields' : ''}>
+                {item.children.map((child) => (
+                  <li className="field" key={child.field}>
+                    <span>{child.field}</span>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      </LinkText>
+    ))
   }
 
   return (
@@ -124,32 +230,7 @@ export const SideMenu: FC = () => {
         </div>
         <div className="side-menu__items">
           <div className="icons">
-            {menuItems.map((item: IMenuItem) => (
-              <LinkText href={item.pathname} key={item.pathname}>
-                <div
-                  className={
-                    router.pathname === item.pathname ? 'active-icons' : 'icon'
-                  }
-                >
-                  <span className="svgIcon">{item.svg}</span>
-                  <span className="name">{item.name}</span>
-                  <span className="arrow">
-                    {item.children && <ArrowOpenIcon />}
-                  </span>
-                  {item.children && (
-                    <>
-                      <ul className={item.children?.length ? 'fields' : ''}>
-                        {item.children.map((child) => (
-                          <li className="field" key={child.field}>
-                            <span>{child.field}</span>
-                          </li>
-                        ))}
-                      </ul>
-                    </>
-                  )}
-                </div>
-              </LinkText>
-            ))}
+            {handleItems(isSuperAdmin ? adminMenuItems : userMenuItems)}
           </div>
           <div onClick={toggleSideBar} aria-hidden className="open-side-menu">
             <span />
